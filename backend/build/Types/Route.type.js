5e95926de0644f85ce341fb4d437384a12bb0b8b
SzymonG110 <gornikowskiszymon@gmail.com>
2022-08-28 17:46:44 +0200

    remove build

--- backend/build/Types/Route.type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Route {
-    constructor() {
-        this.route = '/';
-        this.methods = [];
-    }
-}
-exports.default = Route;
