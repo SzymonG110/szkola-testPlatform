cfb7e31b183910e4d4444625a4456fed94ebdbb4
SzymonG110 <gornikowskiszymon@gmail.com>
2022-08-31 19:10:08 +0200

    .

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -3,9 +3,12 @@ import {Link} from 'react-router-dom'
 const Home = () => {
 
     return (
-        <div className='flex flex-col'>
-            <Link to='/test' className='cursor-pointer hover:text-ownBlueHover duration-500'>Rozpocznij test</Link>
-            <Link to='/questions' className='cursor-pointer hover:text-ownBlueHover duration-500'>Zobacz pytania i odpowedzi</Link>
+        <div className='flex justify-center items-center fixed h-screen w-screen'>
+            <div className='flex flex-col bg-gray-200 p-24 text-xl font-bold text-center rounded-full'>
+                <Link to='/test' className='text-2xl font-extrabold cursor-pointer hover:text-ownBlueHover duration-500'>Rozpocznij test</Link>
+                <Link to='/questions' className='cursor-pointer hover:text-ownBlueHover duration-500'>Zobacz pytania i odpowedzi</Link>
+                <Link to='/stats' className='cursor-pointer hover:text-ownBlueHover duration-500'>Zobacz statystyki</Link>
+            </div>
         </div>
     )
 
